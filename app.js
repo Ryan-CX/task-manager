0a51aaef2ba3e7a0b6a0212450a08fb07ad34817
Ryan (Chenguang) Xu <xcg1234@gmail.com>
2021-11-04 22:18:22 -0400

    updated

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
+const route = require('./routes/tasks');
+
 //routes
 app.get('/hello', (req, res) => {
 	res.send('task manager app');
 });
-
-const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
 	console.log(`Listening on port ${port}`);
