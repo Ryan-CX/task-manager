8ecd6a68c98a729ef74753263315adc07b410e1e
Ryan (Chenguang) Xu <xcg1234@gmail.com>
2021-11-11 11:45:05 -0500

    finished CRUD operations

--- controller/tasks.js
+++ controller/tasks.js
@@ -1,24 +1,74 @@
 const Task = require('../models/task');
 
-const getAllTasks = (req, res) => {
-	res.send('all items');
+const getAllTasks = async (req, res) => {
+	try {
+		const tasks = await Task.find({});
+		res.status(200).json({ tasks });
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
 };
 
 const createTask = async (req, res) => {
-	const task = await Task.create(req.body);
-	res.status(201).json({ task });
+	try {
+		const task = await Task.create(req.body);
+		res.status(201).json({ task });
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+		//catch is for errors that are not handled by the code, to prevent indefinite loading.
+	}
 };
 
-const getTask = (req, res) => {
-	res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+	try {
+		const { id: taskID } = req.params;
+		const task = await Task.findOne({ _id: taskID });
+		//if not match
+		if (!task) {
+			return res
+				.status(404)
+				.json({ error: `Task not found with id: ${taskID}` });
+		}
+
+		res.status(200).json({ task });
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
 };
 
-const updateTask = (req, res) => {
-	res.send('update item');
+const updateTask = async (req, res) => {
+	try {
+		const { id: taskID } = req.params;
+		const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+			new: true,
+			runValidators: true,
+		});
+		//if not match
+		if (!task) {
+			return res
+				.status(404)
+				.json({ error: `Task not found with id: ${taskID}` });
+		}
+
+		res.status(200).json({ id: taskID, data: req.body });
+	} catch (error) {}
 };
 
-const deleteTask = (req, res) => {
-	res.send('delete item');
+const deleteTask = async (req, res) => {
+	try {
+		const { id: taskID } = req.params;
+		const task = await Task.findOneAndDelete({ _id: taskID });
+		//if not match
+		if (!task) {
+			return res
+				.status(404)
+				.json({ error: `Task not found with id: ${taskID}` });
+		}
+
+		res.status(200).json({ task });
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
 };
 
 module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
--- models/Task.js
+++ models/Task.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 const TaskSchema = new mongoose.Schema({
-	name: String,
-	completed: Boolean,
+	name: {
+		//add validation to avoid empty task
+		type: String,
+		required: [true, 'Task name is required'],
+		trim: true,
+		maxlength: [20, 'Task name cannot be more than 20 characters'],
+	},
+	completed: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model('Task', TaskSchema);
